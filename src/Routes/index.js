1d88c3237869e704d28dba23efb3129f22ad68e1
metagenes <48299631+metagenes@users.noreply.github.com>
2020-01-02 16:21:26 +0700

    deploy

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -11,10 +11,7 @@ const Router = express.Router();
 
 Router.get('/', (req, res) => {
     res.json({
-        message: "Welcome to Point Of Sales RESTful API, You can read the documentation at README.md",
-        author: "Irsyaad Budi Prasetianto",
-        email: "irsyaad.budip@gmail.com",
-        github: "github.com/irsyaadbp"
+        message: "Point of Sale API"
     });
 })
 
